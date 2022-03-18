fd903c10db4f354be5747e79ae3d954d6139eea1
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-03-18 19:43:01 -0400

    Initial commit

---
+++ character.py
@@ -0,0 +1,6 @@
+class Character:
+    def __init__(self, name, health, attack_power, attack_names):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+        self.attack_names = attack_names
