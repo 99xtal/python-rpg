266acea2f88d70f70da368227c8cc9cace4a4c05
joerybarczyk <joerybarczyk99@gmail.com>
2022-03-19 14:06:31 -0400

    Initial attack() -Added input attack, random attack functions to character class -Added take damage function to character class -Initial console formatting (needs work)

--- character.py
+++ character.py
@@ -1,6 +1,43 @@
+import random
+import math
+
+def list_attack_names(attack_names):
+    attack_str = ''
+    for i in attack_names:
+        attack_str += f"\t[{i}]\n"
+    return attack_str
+
+
+def generate_attack_damage(attack_power):
+    d20_roll = random.random()
+    return int(attack_power * d20_roll)
+
 class Character:
     def __init__(self, name, health, attack_power, attack_names):
         self.name = name
+        self.max_health = health
         self.health = health
         self.attack_power = attack_power
         self.attack_names = attack_names
+        self.is_alive = True
+
+    def input_attack(self):
+        attack_choice = input(f"\tHow will you attack your foe?\n\n\t{list_attack_names(self.attack_names)}\n\n\t>>> ")
+        print(f"\t\n{self.attack_names[attack_choice]}\n")
+        return generate_attack_damage(self.attack_power)
+
+    def random_attack(self):
+        random_attack_choice = random.randrange(0,len(self.attack_names))
+        attack_name_key_list = list(self.attack_names)
+        random_attack_name = attack_name_key_list[random_attack_choice]
+        print(f"\t\n{random_attack_name}\n")
+        return generate_attack_damage(self.attack_power)
+
+
+    def take_damage(self, damage):
+        if (damage > self.health):
+            self.health = 0
+            self.is_alive = False
+        else:
+            self.health -= damage
+        print(f"\t{self.name} takes {damage}hp of damage!\tHealth Remaining: ({self.health}/{self.max_health}\n")
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-title_text = '''
-###################################################################################################
+from character import Character
 
-   ▄█    █▄       ▄████████    ▄████████  ▄████████ ███    █▄   ▄█          ▄████████    ▄████████ 
-  ███    ███     ███    ███   ███    ███ ███    ███ ███    ███ ███         ███    ███   ███    ███ 
-  ███    ███     ███    █▀    ███    ███ ███    █▀  ███    ███ ███         ███    █▀    ███    █▀  
- ▄███▄▄▄▄███▄▄  ▄███▄▄▄      ▄███▄▄▄▄██▀ ███        ███    ███ ███        ▄███▄▄▄       ███        
-▀▀███▀▀▀▀███▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███        ███    ███ ███       ▀▀███▀▀▀     ▀███████████ 
-  ███    ███     ███    █▄  ▀███████████ ███    █▄  ███    ███ ███         ███    █▄           ███ 
-  ███    ███     ███    ███   ███    ███ ███    ███ ███    ███ ███▌    ▄   ███    ███    ▄█    ███ 
-  ███    █▀      ██████████   ███    ███ ████████▀  ████████▀  █████▄▄██   ██████████  ▄████████▀  
-                              ███    ███                       ▀                                   
+def attack(character1, character2):
+	print(f'''
+	####################################################################
+	{character1.name.upper()} FACES THE {character2.name.upper()}...
+	####################################################################''')
+	
+	while(True):
+		character2.take_damage(character1.input_attack())
+		if not character2.is_alive:
+			return character1
+		character1.take_damage(character2.random_attack())
+		if not character1.is_alive:
+			return character2
 
-###################################################################################################
-    CREATED BY: JOE RYBARCZYK                                                       DEVCODECAMP'''
 
-print(title_text)
+def run_game():
+	# Build Hercules and enemies
+	hercules = Character("Hercules", 100, 50, {"Furious Blade":"You strike at the heart of the beast with a rage that knows no bounds", "Olympian Strike":"You slash at the beast", "Thunder of Zeus": "Lightning strkes as you lift your sword to the sky"})
+	lion = Character("Nemean Lion", 75, 25, {"Claw Strike":"The lion strikes you with his claw", "Power Bite": "The lion bites you"})
+
+	# Hercules fights the lion
+	hercules_vs_lion = attack(hercules, lion)
+
+
+run_game()
